2109af053e1dd19d05cbbc481a7c73c839c70978
Reed Murphy <reed@reedmurphy.net>
2012-11-02 15:38:56 +1100

    Multiple timespans

--- generate.php
+++ generate.php
@@ -3,49 +3,46 @@ require_once('./conf/config.php');
 
 foreach ($config['hostnames'] as $hostname) {
     foreach ($graphs as $graph_name => $graph_config) {
-        $image_filename = $config['img_dir'] . "/" . $hostname . "/" . $graph_name . ".png";
-        $rrd_filename = $config['rrd_dir'] . "/" . $hostname . "/" . $graph_config['file'];
+        foreach ($timespans as $timespan) {
+            $image_filename = $config['img_dir'] . "/" . $hostname . "/" . $graph_name . "_" . $timespan . ".png";
+            $rrd_filename = $config['rrd_dir'] . "/" . $hostname . "/" . $graph_config['file'];
 
-        $options = $graph_defaults;
+            $options = array_merge($graph_defaults, array(
+                "--title", $graph_config['title'],
+                "--start", $timespan,
+            ));
 
-        $options[] = "--title";
-        $options[] = $graph_config['title'];
+            foreach ($graph_config['lines'] as $line_name => $line) {
+                $line = array_merge($graph_config, $line);
 
-        foreach ($graph_config['lines'] as $line_name => $line) {
-            foreach ($graph_config as $key => $value) {
-                if ($key == "lines") continue;
-                if (!array_key_exists($key, $line)) {
-                    $line[$key] = $value;
+                if (!array_key_exists('legend', $line)) {
+                    $line['legend'] = ucwords($line_name);
                 }
+                if (!array_key_exists('ds', $line)) {
+                    $line['ds'] = 'value';
+                }
+                $line_rrd_filename = $config['rrd_dir'] . "/" . $hostname . "/" . $line['file'];
+                $options[] = "DEF:{$line_name}={$line_rrd_filename}:{$line['ds']}:AVERAGE";
+
+                if (array_key_exists('area', $line) && $line['area']) {
+                    $line_def = "AREA:";
+                } else {
+                    $line_def = "LINE{$line['width']}:";
+                }
+                $line_def .= "{$line_name}#{$line['color']}:{$line['legend']}";
+                if (array_key_exists('stacked', $line) && $line['stacked']) {
+                    $line_def .= ":STACK";
+                }
+                $options[] = $line_def;
             }
 
-            if (!array_key_exists('legend', $line)) {
-                $line['legend'] = ucwords($line_name);
+            $graph = rrd_graph($image_filename, $options, count($options));
+            if (DEBUG) {
+                var_export($options);
+                echo "\n";
+                var_export($graph);
+                echo "\n---\n";
             }
-            if (!array_key_exists('ds', $line)) {
-                $line['ds'] = 'value';
-            }
-            $line_rrd_filename = $config['rrd_dir'] . "/" . $hostname . "/" . $line['file'];
-            $options[] = "DEF:{$line_name}={$line_rrd_filename}:{$line['ds']}:AVERAGE";
-
-            if (array_key_exists('area', $line) && $line['area']) {
-                $line_def = "AREA:";
-            } else {
-                $line_def = "LINE{$line['width']}:";
-            }
-            $line_def .= "{$line_name}#{$line['color']}:{$line['legend']}";
-            if (array_key_exists('stacked', $line) && $line['stacked']) {
-                $line_def .= ":STACK";
-            }
-            $options[] = $line_def;
-        }
-
-        $graph = rrd_graph($image_filename, $options, count($options));
-        if (DEBUG) {
-            var_export($options);
-            echo "\n";
-            var_export($graph);
-            echo "\n---\n";
         }
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,28 @@
-<?php require_once('./conf/config.php'); ?>
+<?php require_once('./conf/config.php');
+$timespan = isset($_REQUEST['ts']) ? $_REQUEST['ts'] : $config['default_timespan'];
+?>
 <!doctype html>
 <title>status</title>
 <script> window.setTimeout(function() { window.location.reload(); },<?php echo $config['refresh_minutes']*60000; ?>); </script>
+<style type="text/css">
+ul#menu {
+    list-style: none;
+    margin: 0;
+}
+
+ul#menu li {
+    display: inline;
+    padding-left: 1em;
+}
+</style>
+<ul id="menu">
+<?php foreach($timespans as $t): ?>
+    <li><a href="?ts=<?php echo $t; ?>"><?php echo $t; ?></a></href>
+<?php endforeach; ?>
+</ul>
 <?php foreach ($config['hostnames'] as $hostname): ?>
 <h1><?php echo $hostname; ?></h1>
     <?php foreach ($graphs as $graphname => $graphconfig): ?>
-        <img src="<?php echo $config['img_dir'] . "/" . $hostname . "/" . $graphname . ".png"; ?>" />
+        <img src="<?php echo $config['img_dir'] . "/" . $hostname . "/" . $graphname . "_" . $timespan . ".png"; ?>" />
     <?php endforeach; ?>
 <?php endforeach; ?>
